16f5097910b7b2dd9616ee7c863edda9d5bb89fa
interactive-cache-8375 <randomblue1@yahoo.com>
2019-10-01 21:57:58 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,10 +1,9 @@
 #def find_element_index(array, value_to_find)
-  #count = 0 
-  #while count < array.length do 
-  #if array[count] == value_to_find
-    #return count
-  
-  #count += 1 
+#count = 0 
+#while count < array.length do 
+#if array[count] == value_to_find
+#return count
+#count += 1 
 #end 
 #nil 
 #end
